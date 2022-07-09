9da83eb1dd56caeddcaff3ab0d1a2197ad9a67f1
Usman Gavandi <74993022+usman5445@users.noreply.github.com>
2022-07-09 21:04:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { json } = require("express");
 const express = require("express");
+const PORT = process.env.PORT || 5000
 const productsRoute = require("./Routes/products-route");
 const ordersRoute = require("./Routes/orders-route");
 const customersRoute = require("./Routes/customers-route");
@@ -8,4 +9,4 @@ app.use(json());
 app.use(productsRoute);
 app.use(ordersRoute);
 app.use(customersRoute);
-app.listen(5000, () => console.log("listening on port 5000"));
+app.listen(PORT, () => console.log(`listening on port PORT`));
