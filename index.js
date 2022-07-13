6aea243774cf762f47f38e7a50670184cce484a0
Usman Gavandi <74993022+usman5445@users.noreply.github.com>
2022-07-14 00:58:54 +0530

    added cors

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const { json } = require("express");
 const express = require("express");
+const cors = require("cors")
 const PORT = process.env.PORT || 5000
 const productsRoute = require("./Routes/products-route");
 const ordersRoute = require("./Routes/orders-route");
 const customersRoute = require("./Routes/customers-route");
 const app = express();
 app.use(json());
+app.use(cors())
 app.use(productsRoute);
 app.use(ordersRoute);
 app.use(customersRoute);
