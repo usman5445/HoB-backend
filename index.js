e0c274a6c9d7ef9fdd463665c9fbefff994b813d
usman <usmangavandi5445@gmail.com>
2022-07-19 15:55:23 +0530

    cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { json } = require("express");
 const express = require("express");
+const cors = require("cors");
 const PORT = process.env.PORT || 5000;
 const productsRoute = require("./Routes/products-route");
 const ordersRoute = require("./Routes/orders-route");
@@ -7,6 +8,7 @@ const customersRoute = require("./Routes/customers-route");
 const app = express();
 app.use(json());
 app.use(productsRoute);
+app.use(cors());
 app.use(ordersRoute);
 app.use(customersRoute);
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
